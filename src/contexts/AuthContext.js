8554aba7215bc8d17dc152ad556b2c707145c8b0
k-metra <metrakurt@gmail.com>
2025-10-07 15:41:57 +0800

    feat(auth): add token and username getter and setters for AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,13 +1,24 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState,useEffect } from 'react'
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [authenticated, setAuthenticated] = useState(false);
     const [username, setUsername] = useState("");
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        const savedToken = sessionStorage.getItem("token");
+        const savedUsername = sessionStorage.getItem("username");
+
+        if (savedToken && savedUsername) {
+            // TODO: Verify token with backend, then set authenticated to true if valid
+        }
+    })
+
 
     return (
-        <AuthContext.Provider value={{authenticated, setAuthenticated, username, setUsername}}>
+        <AuthContext.Provider value={{authenticated, setAuthenticated, username, setUsername, token, setToken }}>
             { children }
         </AuthContext.Provider>
     )
