b4a3e7645baae317bb9acbbe7fe433e4ceea8fc2
k-metra <metrakurt@gmail.com>
2025-10-08 10:40:58 +0800

    feat(settings): configure CORS to allow credentials and custom headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,6 +135,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+]
+
+CORS_ALLOWED_CREDENTIALS = True
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'Session-Token',
 ]
 
 AUTH_USER_MODEL = "users.User"
