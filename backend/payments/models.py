676c5a86313003d247d4813e842293b23218378c
k-metra <metrakurt@gmail.com>
2025-10-08 18:00:19 +0800

    fix(payment): streamline status updates and ensure accurate remaining balance calculation

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -6,19 +6,20 @@ class Payment(models.Model):
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     amount_due = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField(default=models.functions.Now)
+    remaining_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     status = models.CharField(max_length=50) # status is either Completed, Pending or Inactive
 
     def __str__(self):
         return f"Payment by {self.payer} of {self.amount_paid} on {self.payment_date.strftime('%Y-%m-%d %H:%M:%S')}"
     
     def save(self, *args, **kwargs):
-        # Automatically update status before updating:
-        if self.amount_paid >= self.amount_due:
-            self.status = "Completed"
-        elif self.amount_paid > 0:
-            self.status = "Pending"
-        else:
-            self.status = "Inactive"
-        
         self.amount_paid = min(self.amount_paid, self.amount_due)  # Prevent overpayment
+        self.remaining_balance = abs(self.amount_due - self.amount_paid)
+
+        # Update status based on payment amounts
+        self.status = "Completed" if self.remaining_balance == 0 else "Pending"
+
+        # Status is rendered as 'Inactive' if amount paid is 0
+        self.status = "Inactive" if self.amount_paid == 0 else self.status
+
         super().save(*args, **kwargs)
