3af36292b53f9b0d5c8651bc7dc26c661ab012c0
k-metra <metrakurt@gmail.com>
2025-10-08 11:10:31 +0800

    fix(login): update API endpoint for login request

--- frontend/src/components/login/loginForm.js
+++ frontend/src/components/login/loginForm.js
@@ -17,7 +17,7 @@ export default function LoginForm() {
     async function onSubmit(e) {
         e.preventDefault();
 
-        await fetch('http://localhost:8000/users/login-api/', {
+        await fetch('http://localhost:8000/api/login-api/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
