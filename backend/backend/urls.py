adb22e6d748a244fbc83ecc72a85ec9fc9fa387f
k-metra <metrakurt@gmail.com>
2025-10-08 11:10:13 +0800

    fix(urls): correct API path for user and session routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('api/', include('users.urls')),
+    path('api/', include('user_sessions.urls')),
 ]
