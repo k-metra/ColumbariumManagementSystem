8cd21bb7624a2c70ac620ce616ea734ba60b217a
k-metra <metrakurt@gmail.com>
2025-10-08 18:00:33 +0800

    feat(payment): add edit link to url routing

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('list-all/', list_payments, name='list_payments'),
     path('create-new/', create_payment, name='create_payment'),
     path('delete/', delete_payment, name='delete_payment'),
+    path('edit/', edit_payment, name='edit_payment'),
 ]
