d9f7f1511da9f8f3cc87a2e2202e6c0e336e74f5
k-metra <metrakurt@gmail.com>
2025-10-09 20:04:43 +0800

    fix: add user and role to audit serializer

--- backend/audit/serializers.py
+++ backend/audit/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import AuditLog
 
 class AuditLogSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username', read_only=True)
+    role = serializers.CharField(source='user.role', read_only=True )
+
     class Meta:
         model = AuditLog
         fields = '__all__'
