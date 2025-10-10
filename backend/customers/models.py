1bd6d1f202cb439aaba464d0b500f6843c12c89c
k-metra <metrakurt@gmail.com>
2025-10-10 17:01:28 +0800

    feat: implement customer model

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    deceased_name = models.CharField(max_length=255, blank=True, null=True)
+    deceased_date = models.DateField(blank=True, null=True)
+    relationship_to_deceased = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
