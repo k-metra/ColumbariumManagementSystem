55524ac1e79ef475cddd19ce9ac9e3b55d29fff9
k-metra <metrakurt@gmail.com>
2025-10-10 11:55:43 +0800

    feat(uac): create a serializer for user creation

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+from roles.models import Role
+from roles.serializers import RoleField
 
 class UserSerializer(serializers.ModelSerializer):
     role = serializers.CharField(source='role.name', read_only=True)
@@ -14,3 +16,20 @@ class UserSerializer(serializers.ModelSerializer):
             return []
         
         return list(obj.role.permissions.values_list('code', flat=True))
+    
+class UserCreateSerializer(serializers.ModelSerializer):
+    role = RoleField(queryset=Role.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            role=validated_data.get('role')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
