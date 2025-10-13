8b77664ddc436b0c517007e23990704f743be663
k-metra <metrakurt@gmail.com>
2025-10-13 16:49:49 +0800

    feat(serializers): enhance OccupantSerializer to include niche handling in create method

--- backend/occupants/serializers.py
+++ backend/occupants/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from niches.models import Niche
 from .models import Occupant
 
 class OccupantSerializer(serializers.ModelSerializer):
+    # 'required' is false because we are injecting it via context in the view
+    niche = serializers.SlugRelatedField(
+        slug_field="location",
+        read_only=True,
+        required=False)
+
     class Meta:
         model = Occupant
-        fields = '__all__'
+        fields = "__all__"
+
+    def create(self, validated_data):
+        niche = self.context.get('niche') or validated_data.pop('niche', None)
+
+        if not niche:
+            raise serializers.ValidationError({"niche": "Niche must be provided."})
+        return Occupant.objects.create(niche=niche, **validated_data)
