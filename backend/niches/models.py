c2afe4f9539c4f75aa471f6226ee6f10ea465e90
k-metra <metrakurt@gmail.com>
2025-10-24 12:37:25 +0800

    feat(models): implement occupant status update logic in Niche model

--- backend/niches/models.py
+++ backend/niches/models.py
@@ -4,7 +4,26 @@ from django.db import models
 class Niche(models.Model):
     amount = models.PositiveIntegerField()
     location = models.CharField(max_length=255)
-    status = models.CharField(max_length=50, default='Available')  # available, occupied, maintenance, reserved
+    status = models.CharField(max_length=50, default='Available') # available, occupied, maintenance, reserved
+    max_occupants = models.PositiveIntegerField(default=2)
+    type = models.CharField(max_length=50, default='Granite') # Granite, Glass, etc.
 
     def __str__(self):
         return f"{self.amount} - {self.location} ({self.status})"
+    
+    def update_status(self):
+        """Update status based on occupant count"""
+        occupant_count = self.occupants.count()
+        if occupant_count == 0:
+            self.status = 'Available'
+        elif occupant_count >= self.max_occupants:
+            self.status = 'Full'
+        else:
+            self.status = 'Occupied'
+    
+    def save(self, *args, **kwargs):
+        # Don't auto-update status on initial creation (when pk is None)
+        # because occupants relationship won't exist yet
+        if self.pk is not None:
+            self.update_status()
+        super().save(*args, **kwargs)
