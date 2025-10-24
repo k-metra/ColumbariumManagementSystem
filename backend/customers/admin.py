1cdb90251467552818d89649fddfb2153badf779
k-metra <metrakurt@gmail.com>
2025-10-24 12:36:42 +0800

    feat(admin): register Customer model with admin interface

--- backend/customers/admin.py
+++ backend/customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'contact_number', 'deceased_name')
+    list_filter = ('deceased_date', 'relationship_to_deceased')
+    search_fields = ('name', 'email', 'deceased_name')
+    readonly_fields = ('id',)
