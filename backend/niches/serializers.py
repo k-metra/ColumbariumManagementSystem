d1b21c5e98d801424e64811e073aa369ed142c44
k-metra <metrakurt@gmail.com>
2025-10-24 12:37:29 +0800

    feat(serializers): add occupant_count field to NicheSerializer

--- backend/niches/serializers.py
+++ backend/niches/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Niche
 
 class NicheSerializer(serializers.ModelSerializer):
+    occupant_count = serializers.SerializerMethodField()
+    
+    def get_occupant_count(self, obj):
+        """Returns the count of occupants in this niche"""
+        return obj.occupants.count()
+
     class Meta:
         model = Niche
         fields = "__all__"
