80e6a0a6586f47d4091a41c5475b0e1f19eddc13
k-metra <metrakurt@gmail.com>
2025-10-24 12:37:38 +0800

    feat(models): streamline niche status updates on occupant save and delete

--- backend/occupants/models.py
+++ backend/occupants/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from niches.models import Niche
 from django.dispatch import receiver
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 
 # Create your models here.
 class Occupant(models.Model):
@@ -11,10 +11,17 @@ class Occupant(models.Model):
 
     def __str__(self):
         return self.name
-    
-@receiver(post_delete, sender=Occupant)
-def set_available_on_delete(sender, instance, **kwargs):
+
+@receiver(post_save, sender=Occupant)
+def update_niche_status_on_save(sender, instance, **kwargs):
+    """Update niche status when an occupant is created or updated"""
     if instance.niche:
-        niche = instance.niche
-        niche.status = "Available"
-        niche.save()
+        instance.niche.update_status()
+        instance.niche.save()
+
+@receiver(post_delete, sender=Occupant)
+def update_niche_status_on_delete(sender, instance, **kwargs):
+    """Update niche status when an occupant is deleted"""
+    if instance.niche:
+        instance.niche.update_status()
+        instance.niche.save()
