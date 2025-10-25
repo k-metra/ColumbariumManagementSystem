a6603a59c9d33c5ccb97ca2a8b17a68a31454de3
k-metra <metrakurt@gmail.com>
2025-10-25 16:39:32 +0800

    feat(home): increase map iframe width for better visibility

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -247,7 +247,7 @@ export default function HomePage() {
                          <div className="w-full flex justify-center items-center mt-8">
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3864.4471549278214!2d121.03256567414589!3d14.401367681925306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d05360582529%3A0xedc43c6e31a52381!2sMary%2C%20Cause%20of%20Our%20Joy%20Parish!5e0!3m2!1sen!2sph!4v1761380995133!5m2!1sen!2sph"
-                                    width="600"
+                                    width="800"
                                     height="450"
                                     style={{ border: 0 }}
                                     allowFullScreen=""
